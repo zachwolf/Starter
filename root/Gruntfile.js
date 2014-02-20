b29dd0b5a7d0fe5e93ce0d1ce4494c1d94d2c4e3
Zach Wolf <hello@zachwolf.com>
2014-02-19 22:11:11 -0600

    simplified gruntfile

--- root/Gruntfile.js
+++ root/Gruntfile.js
@@ -1,87 +1,17 @@
 // task settings
-var GRUNT_TASKS_PATH  = "./grunt-tasks",
-    SETTINGS = require( GRUNT_TASKS_PATH + "/globalSettings" );
+var SETTINGS = require( "./grunt-tasks/settings" );
 
 module.exports = function(grunt) {
-
-  // Project configuration.
-  grunt.initConfig({
-    karma: {
-      unit: {
-        singleRun: true,
-        configFile: 'karma.conf.js'
-      }
-    },
-    compass: {
-      dist: {
-        options: {
-          outputStyle: 'compressed',
-          noLineComments: true
-        }
-      },
-      dev: {
-        options: {
-          outputStyle: 'expanded',
-          watch: false
-        }
-      }
-    },
-    jshint: {
-      dist: {
-        options: SETTINGS.JSHINT_DIST_SETTINGS,
-        files: {
-          src: [ SETTINGS.SCRIPT_SOURCE_PATH + '/*.js', SETTINGS.SCRIPT_SOURCE_PATH + '/**/*.js',
-                 "!" + SETTINGS.SCRIPT_SOURCE_PATH + '/lib/*.js',
-                 "!" + SETTINGS.SCRIPT_SOURCE_PATH + '/test/*.js']
-        }
-      },
-      dev: {
-        options: SETTINGS.JSHINT_DEV_SETTINGS,
-        files: {
-          src: [ SETTINGS.SCRIPT_SOURCE_PATH + '/*.js', SETTINGS.SCRIPT_SOURCE_PATH + '/**/*.js',
-                 "!" + SETTINGS.SCRIPT_SOURCE_PATH + '/lib/*.js']
-        }
-      }
-    },
-    connect: {
-      local: {
-        options: {
-          port: 9000,
-          base: SETTINGS.BUILD_PATH,
-          open: true
-        }
-      }
-    },
-    copy: {
-      scripts: {
-        files: [
-          {
-            // copy all js and directories except our tests
-            src: [ '**', '!test/**' ],
-            expand: true,
-            cwd: SETTINGS.SCRIPT_SOURCE_PATH + '/',
-            dest: SETTINGS.SCRIPT_BUILD_PATH
-          }
-        ]
-      }
-    },
-    requirejs: {
-      compile: {
-        options: {
-          name: "{%= primary_js_name %}",
-          baseUrl: SETTINGS.SCRIPT_SOURCE_PATH,
-          mainConfigFile: SETTINGS.SCRIPT_SOURCE_PATH + "/{%= primary_js_name %}.js",
-          out: SETTINGS.SCRIPT_BUILD_PATH + "/{%= primary_js_name %}.js"
-        }
-      }
-    },
-    clean: [ SETTINGS.BUILD_PATH ]
-  });
 
   // load all required grunt tasks dependencies
   require('load-grunt-tasks')(grunt);
 
+  // alias watch task to allow for prompt
+  grunt.task.renameTask("watch", "_watch");
+
   // load tasks from the grunt tasks dir
-  grunt.task.loadTasks(GRUNT_TASKS_PATH);
+  grunt.task.loadTasks(SETTINGS.GRUNT_TASKS_PATH);
+  grunt.task.loadTasks(SETTINGS.GRUNT_TASKS_PATH + '/asset-tasks');
+  grunt.task.loadTasks(SETTINGS.GRUNT_TASKS_PATH + '/server-tasks');
 
 };
